0d52b277f78bb6695ede81560c69b8df0388c2f7
Anna Walkowska <anna@propergate.co>
2024-10-07 14:27:04 +0200

    clean to start working

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,35 @@ import { defineConfig } from "vite";
 import { globSync } from "glob";
 
 const writeIndexHTML = () => {
-  let links: string = "";
+  let links: string = "
+        <a style="width: fit-content;" href="./packages\front\src\measurement\VolumeMeasurement\example.html">front/VolumeMeasurement</a>
+      <a style="width: fit-content;" href="./packages\front\src\measurement\LengthMeasurement\example.html">front/LengthMeasurement</a>
+      <a style="width: fit-content;" href="./packages\front\src\measurement\FaceMeasurement\example.html">front/FaceMeasurement</a>
+      <a style="width: fit-content;" href="./packages\front\src\measurement\EdgeMeasurement\example.html">front/EdgeMeasurement</a>
+      <a style="width: fit-content;" href="./packages\front\src\measurement\AreaMeasurement\example.html">front/AreaMeasurement</a>
+      <a style="width: fit-content;" href="./packages\front\src\measurement\AngleMeasurement\example.html">front/AngleMeasurement</a>
+      <a style="width: fit-content;" href="./packages\front\src\fragments\Sections\example.html">front/Sections</a>
+      <a style="width: fit-content;" href="./packages\front\src\fragments\Plans\example.html">front/Plans</a>
+      <a style="width: fit-content;" href="./packages\front\src\fragments\IfcStreamer\example.html">front/IfcStreamer</a>
+      <a style="width: fit-content;" href="./packages\front\src\fragments\Highlighter\example.html">front/Highlighter</a>
+      <a style="width: fit-content;" href="./packages\front\src\core\PostproductionRenderer\example.html">front/PostproductionRenderer</a>
+      <a style="width: fit-content;" href="./packages\core\src\openbim\BCFTopics\example.html">core/BCFTopics</a>
+      <a style="width: fit-content;" href="./packages\core\src\measurement\MeasurementUtils\example.html">core/MeasurementUtils</a>
+      <a style="width: fit-content;" href="./packages\core\src\ifc\IfcRelationsIndexer\example.html">core/IfcRelationsIndexer</a>
+      <a style="width: fit-content;" href="./packages\core\src\ifc\IfcPropertiesManager\example.html">core/IfcPropertiesManager</a>
+      <a style="width: fit-content;" href="./packages\core\src\ifc\IfcJsonExporter\example.html">core/IfcJsonExporter</a>
+      <a style="width: fit-content;" href="./packages\core\src\fragments\IfcLoader\example.html">core/IfcLoader</a>
+      <a style="width: fit-content;" href="./packages\core\src\fragments\Hider\example.html">core/Hider</a>
+      <a style="width: fit-content;" href="./packages\core\src\fragments\FragmentsManager\example.html">core/FragmentsManager</a>
+      <a style="width: fit-content;" href="./packages\core\src\fragments\Exploder\example.html">core/Exploder</a>
+      <a style="width: fit-content;" href="./packages\core\src\fragments\Classifier\example.html">core/Classifier</a>
+      <a style="width: fit-content;" href="./packages\core\src\fragments\BoundingBoxer\example.html">core/BoundingBoxer</a>
+      <a style="width: fit-content;" href="./packages\core\src\core\Worlds\example.html">core/Worlds</a>
+      <a style="width: fit-content;" href="./packages\core\src\core\Raycasters\example.html">core/Raycasters</a>
+      <a style="width: fit-content;" href="./packages\core\src\core\OrthoPerspectiveCamera\example.html">core/OrthoPerspectiveCamera</a>
+      <a style="width: fit-content;" href="./packages\core\src\core\MiniMap\example.html">core/MiniMap</a>
+      <a style="width: fit-content;" href="./packages\core\src\core\Grids\example.html">core/Grids</a>
+      ";
   const examplePaths = globSync("packages/**/src/**/example.html");
   for (const examplePath of examplePaths) {
     const directory = path.dirname(examplePath);
